fa6e03855b658a530b433d9cc35080773a9a001b
Marcus Voss <voss.marcus@gmail.com>
2020-12-30 07:55:35 +0100

    renamed package to pytwed

--- setup.py
+++ setup.py
@@ -4,14 +4,13 @@ from Cython.Distutils import build_ext
 import numpy
 
 setup(
-    name="twed",
+    name="pytwed",
     version="1.0",
-    license="GPL",
+    license="GPLv3",
     author="Pierre-Francois Marteau, JZ, Marcus Voss",
     cmdclass={'build_ext': build_ext},
-    ext_modules=[Extension('twed', sources = ['src/twed_wrap.c'], include_dirs=[numpy.get_include()])],
+    ext_modules=[Extension('twed', sources = ['pytwed/twed_wrap.c'], include_dirs=[numpy.get_include()])],
     include_dirs=[numpy.get_include()],
     description="This is a python wrapper for Marteau's implementation of his 2009 paper introducing TWED.",
-   # packages=["twed"],
     url = 'https://github.com/jzumer/pytwed'
 )
