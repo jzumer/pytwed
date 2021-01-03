f32fb3d582fd3d8d4a145c5d319608a37daa6047
Marcus Voss <voss.marcus@gmail.com>
2021-01-03 13:07:28 +0100

    minor changes to meta info

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup, find_packages
 from distutils.extension import Extension
 import numpy
+from Cython.Distutils import build_ext
 
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
@@ -8,17 +9,21 @@ with open("README.md", "r") as readme_file:
 setup(
     name="pytwed",
     version="1.0",
-    license="GPLv3",
+    license="MIT License",
     author="Pierre-Francois Marteau, JZ, Marcus Voss",
-    ext_modules=[Extension('twed', sources = ['pytwed/twed_wrap.c'], include_dirs=[numpy.get_include()])],
-    description="This is a python wrapper for Marteau's implementation of his 2009 paper introducing TWED.",
+    setup_requires=["numpy"],
+    ext_modules=[Extension('twed', sources = ['pytwed/twed_wrap.c'], include_dirs=[numpy.get_include()], language="c")],
+    description="This is a python wrapper for Marteau's reference implementation of his 2009 paper introducing the Time Warped Edit Distance (TWED).",
     long_description=readme,
     long_description_content_type="text/markdown",
     url = 'https://github.com/jzumer/pytwed',
     packages=find_packages(),
+    cmdclass={'build_ext': build_ext},
     classifiers=[
-        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3",
         "Programming Language :: C",
         "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering"
     ],
 )
