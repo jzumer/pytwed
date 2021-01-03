05bfb96a73e63f61840f5dea0d4a37b21674bb4f
Marcus Voss <voss.marcus@gmail.com>
2021-01-03 12:41:30 +0100

    changed licence to MIT

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from distutils.extension import Extension
 import numpy
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
 
 setup(
     name="pytwed",
@@ -9,5 +12,13 @@ setup(
     author="Pierre-Francois Marteau, JZ, Marcus Voss",
     ext_modules=[Extension('twed', sources = ['pytwed/twed_wrap.c'], include_dirs=[numpy.get_include()])],
     description="This is a python wrapper for Marteau's implementation of his 2009 paper introducing TWED.",
-    url = 'https://github.com/jzumer/pytwed'
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url = 'https://github.com/jzumer/pytwed',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: C",
+        "License :: OSI Approved :: MIT License",
+    ],
 )
