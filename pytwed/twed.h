8be50864d6762a9bc359af0b227df76d664ff06f
Jeremie Zumer <jeremie_zumer@hotmail.com>
2021-01-05 12:46:21 -0500

    Fix missing header/bad #include in fast_twed.c

---
+++ pytwed/twed.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <math.h>
+
+double DTWEDL1d(int n_feats, double* arr1, int arr1_lgt, double* arr1_spec, double* arr2, int arr2_lgt, double* arr2_spec, double nu, double lambda, int degree);
